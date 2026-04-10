2dc5411e6c531039ab8a0f8d7c78f6e34819f9c8
est.natanrcs <est.natanrcs@JSMC02F659HQ05N.local>
2026-04-10 17:01:09 -0300

    update system

--- teste.py
+++ teste.py
@@ -1,5 +1,5 @@
 DATABASE = []
-def cadastro_usuario():
+def cadastrar():
     nome = input('digite seu nome...')
     idade = int(input('digite sua idade...'))
     ativo = True
@@ -10,4 +10,36 @@ def cadastro_usuario():
     }
     DATABASE.append(user)
     print(DATABASE)
-cadastro_usuario()
+
+def excluir():
+    global DATABASE
+    e = input('nome do cliente:')
+    for i in DATABASE:
+        if i['nome'] == e:
+            DATABASE.remove(i)
+            print('Removido com sucesso',DATABASE)
+
+def main():
+    while True:
+        print('1-cadastrar')
+        print('2-ver usuario')
+        print('3- excluir')
+        print('4-sair')
+        try:
+            a = int(input('Escolha uma opcao: '))
+        except ValueError:
+            print('Apenas numeros.')
+            continue
+        if a ==1:
+            cadastrar()
+        elif a ==2:
+            print(DATABASE)
+        elif a ==3:
+            excluir()
+        elif a ==4:
+            print('saindo')
+            break
+        else:
+            print('opcao invalida')
+main(1)
+
