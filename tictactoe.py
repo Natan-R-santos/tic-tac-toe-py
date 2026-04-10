86cb36c1b6ee87f13fdba4bdb28fddfc3032d8cc
est.natanrcs <est.natanrcs@JSMC02F659HQ05N.local>
2026-04-10 16:35:46 -0300

    refactor: melhoria na estrutura do sistema e lógica do jogo
    - refatoração do código para melhor organização
    - tratamento de erros de entrada
    - separação de responsabilidades em funções
    - melhoria no controle de fluxo do código

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,6 @@
 tabuleiro = [' '] * 9
-jogadordavez = 'O'
+escolha = input('Escolha X ou O: ').upper()
+jogadordavez = escolha
 def exibir_tabuleiro():
     print(f'{tabuleiro[0]} | {tabuleiro[1]} | {tabuleiro[2]}')
     print('---------')
@@ -7,30 +8,49 @@ def exibir_tabuleiro():
     print('---------')
     print(f'{tabuleiro[6]} | {tabuleiro[7]} | {tabuleiro[8]}')
 
-def verificar_ganhador(tabuleiro,jogadordavez):
-    vitorias = [
-        (0,1,2),(3,4,5),(6,7,8),# combinador horizontal
-        (0,3,6),(1,4,7),(2,5,8),# combinador vertical
-        (0,4,8),(2,4,6)# combinador diagonal
-    ]
-    for a,b,c in vitorias:
-        if tabuleiro[a] == jogadordavez and tabuleiro[b] == jogadordavez and tabuleiro[c]== jogadordavez:
+def obter_jogada():
+    while True:
+        try:
+            jogada = int(jogada)
+        except ValueError:
+            print('Apenas numeros.')
+            continue
+        if jogada < 0 or jogada > 8:
+            print('Só os numeros 0 e 8.')
+            continue
+        if tabuleiro[jogada] != ' ':
+            print('Casa está marcada,tente uma casa livre.')
+            continue
+        return jogada
+    
+def ganhador():
+    vitoria =[(0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6)]
+    for a,b,c in vitoria:
+        if tabuleiro[a] == jogadordavez and tabuleiro[b] == jogadordavez and tabuleiro[c] == jogadordavez:
             return True
     return False
 
-while True:
-    exibir_tabuleiro()
-    jogada = int(input('Insira de 0 a 8:'))
-    if tabuleiro[jogada] != ' ':
-        print('CASA OCUPADA TENTE OUTRA')
-        continue
-    tabuleiro[jogada] = jogadordavez 
-    if verificar_ganhador(tabuleiro,jogadordavez):
+def main():
+    global jogadordavez
+    while True:
         exibir_tabuleiro()
-        print(f'Jogador: {jogadordavez} Venceu')
-        break
-    if jogadordavez == 'O':
-        jogadordavez = 'X'
-    else:
-        jogadordavez  ='O'
+        jogada = obter_jogada()
+        tabuleiro[jogada] = jogadordavez
+        if ganhador():
+            exibir_tabuleiro()
+            print(f'{jogadordavez} Ganhou !!')
+            break
+        if ' ' not in tabuleiro:
+            exibir_tabuleiro()
+            print('Empate,Tabuleiro preenchido.')
+            break
+        if jogadordavez == 'O':
+            jogadordavez = 'X'
+        else:
+            jogadordavez = 'O'
+main()
+
+
+
+        
         
